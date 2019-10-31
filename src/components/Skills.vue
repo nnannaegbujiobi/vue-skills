47e3e400aa31fd707a5b0374c37abbdcc465aec2
Nnanna Egbujiobi <nnanna.egbujiobi@ENG-MAC-011.local>
2019-10-31 15:22:25 -0500

    template/methods

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,32 +1,87 @@
 <template>
   <div class="hello">
-   Skills
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+      <input type="text" placeholder="Enter a skill" v-model="skill">
+      {{ skill }} 
+      <input type="checkbox" id="checkbox" v-model="checked">
+      </form>
+      <!-- allows us through interperlate to display input -->
+      <ul>
+        <li v-for="(data, i) in skills" :key="i">{{i+1}}.{{data.skill}}</li>
+      </ul>
+      <p> These are your skills!</p>
+      <!-- <p v-if="skills.length >= 1">You have more than 1 skill</p>
+      <p v-else>You need more skills bro</p> -->
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
-    
+  data() {
+    return {
+      checked: false,
+      skill: '',
+      skills: [
+        {"skill": "Vue.js"},
+        {"skill": "Frontend Developer"},
+        {"skill": "Backend Developer"},
+        {"skill": "Full Stack Developer"}
+      ]
+      
+    }
+  },
+  methods: {
+    addSkill() {
+      this.skills.push({skill: this.skill});
+      this.skill = '';
+      // set skill to empty string so any input would work
+       console.log('This checkbox value is: '+this.checked);
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+ /* if you have a lot of css make a css file and inlude inside style tag  src="./Skills.css" scoped> */
+ .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
+ </style> 
